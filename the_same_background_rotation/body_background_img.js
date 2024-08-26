b39bafe9e1d4628967bf10a051eb54daa54e6540
承諾 <klnaserzy@gmail.com>
2024-08-26 15:11:04 +0800

    test api key

--- the_same_background_rotation/body_background_img.js
+++ the_same_background_rotation/body_background_img.js
@@ -1,6 +1,9 @@
 import { backgroundImages as backgroundImagePath } from "./fetchBackgroundImg.js";
 import { pexels_API_KEY } from "./API_KEY.js";
 
+const a = pexels_API_KEY;
+console.log(a.length);
+
 // DOM 元素
 const photographer = document.getElementById("photographer");
 const nowDate = document.getElementById("nowDate");
